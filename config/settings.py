7cdab904384b5ff006a117801506f7488a9d7d42
ParkMinWoo-hub <136790431+ParkMinWoo-hub@users.noreply.github.com>
2023-06-28 13:38:29 +0900

    App 수정 1. User Model 수정 2. UserAdmin 수정

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 CUSTOM_APPS = [
-    "houses.apps.HousesConfig",
+    "users.apps.UsersConfig",
 ]
 
 SYSTEM_APPS = [
@@ -125,3 +125,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Auth
+AUTH_USER_MODEL = "users.User"
