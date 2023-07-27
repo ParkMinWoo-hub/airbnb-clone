d246ca150f3afc13cd7b4251d501117392a1e866
ParkMinWoo-hub <136790431+ParkMinWoo-hub@users.noreply.github.com>
2023-07-27 19:24:11 +0900

    django
    HTTP Request 처리 Logic을 Function에서 Class화

--- categories/views.py
+++ categories/views.py
@@ -2,14 +2,13 @@ from rest_framework.decorators import api_view
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.views import APIView
 from .models import Category
 from .serializers import CategorySereializer
 
 
-# Create your views here.
-@api_view(["GET", "POST"])
-def categories(request):
-    if request.method == "GET":
+class Categories(APIView):
+    def get(self, request):
         all_categories = Category.objects.all()
         serializer = CategorySereializer(all_categories, many=True)
         return Response(
@@ -18,7 +17,8 @@ def categories(request):
                 "categories": serializer.data,
             },
         )
-    elif request.method == "POST":
+
+    def post(self, request):
         serializer = CategorySereializer(data=request.data)
         if serializer.is_valid():
             new_category = serializer.save()
@@ -27,19 +27,20 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view(["GET", "PUT", "DELETE"])
-def category(request, pk):
-    try:
-        category = Category.objects.get(pk=pk)
-    except Category.DoesNotExist:
-        raise NotFound
+class CategoryDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            raise NotFound
 
-    if request.method == "GET":
-        serializer = CategorySereializer(category)
+    def get(self, request, pk):
+        serializer = CategorySereializer(self.get_object(pk))
         return Response(serializer.data)
-    elif request.method == "PUT":
+
+    def put(self, request, pk):
         serializer = CategorySereializer(
-            category,
+            self.get_object(pk),
             data=request.data,
             partial=True,
         )
@@ -48,6 +49,7 @@ def category(request, pk):
             return Response(CategorySereializer(update_category).data)
         else:
             return Response(serializer.errors)
-    elif request.method == "DELETE":
-        category.delete()
+
+    def delete(self, request, pk):
+        self.get_object(pk).delete()
         return Response(status=HTTP_204_NO_CONTENT)
